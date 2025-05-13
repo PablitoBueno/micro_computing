d1d29d459a76db58a20cb14421c8f29227f3cb47
Pablo Bueno <pablobuenodearaujo@gmail.com>
2025-05-12 21:45:43 -0300

    Update

--- micro_computing.ino
+++ micro_computing.ino
@@ -1,25 +1,12 @@
-#include <avr/io.h>
-#include <util/delay.h>
+#include <Keypad.h>
+#include <LiquidCrystal.h>
 #include <avr/pgmspace.h>
 #include <string.h>
 
-// === LCD 4-bit pins ===
-#define LCD_RS PC1
-#define LCD_RW PC0
-#define LCD_E  PD2
-#define LCD_D4 PC2
-#define LCD_D5 PC3
-#define LCD_D6 PC4
-#define LCD_D7 PC5
-
-// === Keypad pins ===
-const uint8_t rowPins[4] = {PD7, PB0, PB1, PB2}; // 7,8,9,10
-const uint8_t colPins[4] = {PD3, PD4, PD5, PD6}; // 3,4,5,6
-
 #define MAX_CHARS 16
-#define TAP_DELAY 800  // ms
+#define TAP_DELAY 800
+#define BLINK_INTERVAL 500
 
-// Multitap maps
 const char map0[] PROGMEM = "ABC2";
 const char map1[] PROGMEM = "DEF3";
 const char map2[] PROGMEM = "GHI4";
@@ -30,7 +17,9 @@ const char map6[] PROGMEM = "TUV8";
 const char map7[] PROGMEM = "WXYZ9";
 const char map8[] PROGMEM = ".,?!";
 const char map9[] PROGMEM = "-_/;:'";
-const char* const maps[] PROGMEM = { map0,map1,map2,map3,map4,map5,map6,map7,map8,map9 };
+const char* const maps[] PROGMEM = {
+  map0,map1,map2,map3,map4,map5,map6,map7,map8,map9
+};
 
 char buffer[MAX_CHARS+1] = {0};
 uint8_t bufLen = 0;
@@ -38,129 +27,88 @@ uint8_t cursorPos = 0;
 int8_t lastKey = -1;
 uint8_t tapIndex = 0;
 unsigned long lastTapTime = 0;
+bool blinkState = false;
+unsigned long lastBlinkTime = 0;
 
-// === LCD low-level ===
-void lcdPulse() {
-    PORTD |= (1<<LCD_E);
-    _delay_us(1);
-    PORTD &= ~(1<<LCD_E);
-    _delay_us(80);
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {4,5,6,7};
+byte colPins[COLS] = {8,9,10,11};
+Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+LiquidCrystal lcd(A0,A1,A2,A3,A4,A5);
+
+void setup(){
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Notebook:");
 }
 
-void lcdCmd(uint8_t cmd) {
-    PORTC &= ~((1<<LCD_RS)|(1<<LCD_RW));
-    PORTC = (PORTC & 0xC3) | ((cmd>>4)<<2);
-    lcdPulse();
-    PORTC = (PORTC & 0xC3) | ((cmd&0x0F)<<2);
-    lcdPulse();
-    _delay_ms(2);
-}
-
-void lcdData(uint8_t d) {
-    PORTC = (PORTC & ~(1<<LCD_RW)) | (1<<LCD_RS);
-    PORTC = (PORTC & 0xC3) | ((d>>4)<<2);
-    lcdPulse();
-    PORTC = (PORTC & 0xC3) | ((d&0x0F)<<2);
-    lcdPulse();
-    _delay_ms(2);
-}
-
-void lcdInit() {
-    DDRC |= (1<<LCD_RS)|(1<<LCD_RW)|(1<<LCD_D4)|(1<<LCD_D5)|(1<<LCD_D6)|(1<<LCD_D7);
-    DDRD |= (1<<LCD_E);
-    _delay_ms(50);
-    lcdCmd(0x33); lcdCmd(0x32);
-    lcdCmd(0x28); lcdCmd(0x0C);
-    lcdCmd(0x06); lcdCmd(0x01);
-    _delay_ms(2);
-}
-
-// === Keypad ===
-void keypadInit() {
-    DDRD |= (1<<PD7);
-    DDRB |= (1<<PB0)|(1<<PB1)|(1<<PB2);
-    PORTD |= (1<<PD7);
-    PORTB |= (1<<PB0)|(1<<PB1)|(1<<PB2);
-    DDRD &= ~((1<<PD3)|(1<<PD4)|(1<<PD5)|(1<<PD6));
-    PORTD |= (1<<PD3)|(1<<PD4)|(1<<PD5)|(1<<PD6);
-}
-
-int8_t getKey() {
-    for(int8_t r=0;r<4;r++){
-        PORTD |= (1<<PD7);
-        PORTB |= (1<<PB0)|(1<<PB1)|(1<<PB2);
-        if(r==0) PORTD &= ~(1<<PD7);
-        if(r==1) PORTB &= ~(1<<PB0);
-        if(r==2) PORTB &= ~(1<<PB1);
-        if(r==3) PORTB &= ~(1<<PB2);
-        _delay_us(5);
-        for(int8_t c=0;c<4;c++){
-            uint8_t bit = (c<1?PD3:c<2?PD4:c<3?PD5:PD6);
-            if(!(PIND & (1<<bit))) {
-                _delay_ms(50);
-                while(!(PIND & (1<<bit)));
-                return r*4 + c;
-            }
-        }
+void loop(){
+  unsigned long now = millis();
+  if(now - lastBlinkTime >= BLINK_INTERVAL){
+    blinkState = !blinkState;
+    lastBlinkTime = now;
+  }
+  char k = kpd.getKey();
+  if(k){
+    if(k=='*'){
+      if(cursorPos>0){
+        memmove(buffer+cursorPos-1, buffer+cursorPos, bufLen-cursorPos);
+        bufLen--; cursorPos--;
+        buffer[bufLen] = '\0';
+      }
+      lastKey = -1;
     }
-    return -1;
-}
-
-void refreshDisplay() {
-    lcdCmd(0x80);
-    // first line blank
-    for(uint8_t i=0;i<16;i++) lcdData(' ');
-    lcdCmd(0xC0);
-    for(uint8_t i=0;i<16;i++) {
-        if(i==cursorPos && i<bufLen) lcdData('_'); // underline current char
-        else if(i<bufLen) lcdData(buffer[i]);
-        else lcdData(' ');
+    else if(k=='A'){
+      if(cursorPos>0) cursorPos--;
+      lastKey = -1;
     }
-}
-
-int main(void) {
-    lcdInit();
-    keypadInit();
-    refreshDisplay();
-    while(1) {
-        int8_t k = getKey();
-        unsigned long now = (unsigned long)(TCNT1);
-        if(k>=0 && k<10) {
-            const char* ptr = (const char*)pgm_read_ptr(&maps[k]);
-            uint8_t len = strlen_P(ptr);
-            char ch;
-            if(k==lastKey && (now-lastTapTime)<TAP_DELAY && cursorPos>0) {
-                tapIndex = (tapIndex+1)%len;
-                ch = pgm_read_byte(ptr+tapIndex);
-                buffer[cursorPos-1]=ch;
-            } else if(bufLen<MAX_CHARS) {
-                tapIndex=0;
-                ch = pgm_read_byte(ptr);
-                // insert at cursorPos
-                memmove(buffer+cursorPos+1, buffer+cursorPos, bufLen-cursorPos);
-                buffer[cursorPos++] = ch;
-                bufLen++;
-                buffer[bufLen]='\0';
-            }
-            lastKey = k; lastTapTime = now;
-        }
-        else if(k==10) { // key 10 => move left
-            if(cursorPos>0) cursorPos--;
-        }
-        else if(k==11) { // key 11 => move right
-            if(cursorPos<bufLen) cursorPos++;
-        }
-        else if(k==12) { // key 12 => delete at cursor
-            if(cursorPos>0) {
-                memmove(buffer+cursorPos-1, buffer+cursorPos, bufLen-cursorPos);
-                bufLen--; cursorPos--;
-                buffer[bufLen]='\0';
-            }
-        }
-        if(lastKey>=0 && (now-lastTapTime)>=TAP_DELAY) {
-            lastKey=-1; tapIndex=0;
-        }
-        refreshDisplay();
+    else if(k=='B'){
+      if(cursorPos<bufLen) cursorPos++;
+      lastKey = -1;
     }
-    return 0;
+    else if(k>='0' && k<='9'){
+      int idx = k - '0';
+      const char* ptr = (const char*)pgm_read_ptr(&maps[idx]);
+      uint8_t len = strlen_P(ptr);
+      char ch;
+      if(lastKey==idx && now - lastTapTime < TAP_DELAY && cursorPos>0){
+        tapIndex = (tapIndex+1)%len;
+        ch = pgm_read_byte(ptr+tapIndex);
+        buffer[cursorPos-1] = ch;
+      } else if(bufLen < MAX_CHARS){
+        tapIndex = 0;
+        ch = pgm_read_byte(ptr);
+        memmove(buffer+cursorPos+1, buffer+cursorPos, bufLen-cursorPos);
+        buffer[cursorPos++] = ch;
+        bufLen++;
+        buffer[bufLen] = '\0';
+      }
+      lastKey = idx;
+      lastTapTime = now;
+    }
+  }
+  if(lastKey>=0 && now - lastTapTime >= TAP_DELAY){
+    lastKey = -1;
+    tapIndex = 0;
+    if(cursorPos < bufLen) cursorPos++;
+  }
+  lcd.setCursor(0,1);
+  for(uint8_t i=0; i<MAX_CHARS; i++){
+    if(i<bufLen){
+      if(i==cursorPos && blinkState) lcd.print('_');
+      else lcd.print(buffer[i]);
+    } else {
+      if(i==cursorPos && blinkState) lcd.print('_');
+      else lcd.print(' ');
+    }
+  }
 }
